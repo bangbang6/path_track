21534b5af19f4e2fcd0ddf533c8ace8dbe4bbc7c
1768001587 <1768001587@qq.com>
2021-11-12 10:46:21 +0800

    ''

--- src/main.js
+++ src/main.js
@@ -18,8 +18,8 @@ Vue.component('v-chart', VueECharts)
 import './assets/font/iconfont.css'
 import '@/icons' // icon
 import '@/permission' // permission control
-import 'echarts'
-import VueECharts from 'vue-echarts'
+// import 'echarts'
+// import VueECharts from 'vue-echarts'
 Vue.component('v-chart', VueECharts)
 
 /**
@@ -55,12 +55,12 @@ Date.prototype.format = function(fmt){
   if(/(y+)/.test(fmt)){
     fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
   }
-        
+
   for(var k in o){
     if(new RegExp("("+ k +")").test(fmt)){
       fmt = fmt.replace(
-        RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));  
-    }       
+        RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+    }
   }
 
   return fmt;
